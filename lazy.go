3d5f8fad64c7ca1f2acc3f39c3c4d37a2a07b880
Ahmad Sameh <8120013+asmsh@users.noreply.github.com>
2024-09-08 23:34:32 +0400

    add the IsLoaded method to report whether the value has been loaded or not yet

--- lazy.go
+++ lazy.go
@@ -1,6 +1,9 @@
 package lazy
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Value is a generic container offering lazy-loading for values of any type.
 // Safe for concurrent usage.
@@ -15,6 +18,11 @@ type Value[T any] interface {
 	// All callers will block until the value is loaded.
 	Err() error
 
+	// IsLoaded reports whether the lazily-loaded value contained within
+	// this Value has been loaded or not.
+	// It doesn't recognize any ongoing calls and will still return false.
+	IsLoaded() bool
+
 	private()
 }
 
@@ -25,17 +33,18 @@ func NewValue[T any](init func() (T, error)) Value[T] {
 }
 
 type lazyValue[T any] struct {
-	init func() (T, error)
-	once sync.Once
-	val  T
-	err  error
+	init   func() (T, error)
+	once   sync.Once
+	val    T
+	err    error
+	loaded atomic.Uint32
 }
 
 func (lv *lazyValue[T]) load() {
 	lv.once.Do(func() {
 		lv.val, lv.err = lv.init()
-		// release init, so the GC can collect it.
-		lv.init = nil
+		lv.init = nil      // release init, so the GC can collect it.
+		lv.loaded.Store(1) // any non-zero value.
 	})
 }
 
@@ -49,4 +58,8 @@ func (lv *lazyValue[T]) Val() T {
 func (lv *lazyValue[T]) Err() error {
 	lv.load()
 	return lv.err
+}
+
+func (lv *lazyValue[T]) IsLoaded() bool {
+	return lv.loaded.Load() != 0
 }
